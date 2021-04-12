7bf6a060ce680a922652e89d8bf619e4d653a2ce
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-04-13 02:16:47 +0600

    working well

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -43,8 +43,8 @@ const Admin = () => {
     return (
         <div className='w-100 d-flex'>
             <div style={{backgroundColor:'#90EE90',borderRadius:'10px'}} className='w-25 m-5 inventory h-100'>
-                <h5 className='my-5'><Link to='/manageorder' style={{color:'white', textDecoration:'none', backgroundColor:'#198754',padding:'5px',borderRadius:'5px',padding:'8px'}}>Manage Product</Link> </h5>
-                <h5 className='my-5'><Link to='/admin' style={{color:'white', textDecoration:'none',padding:'5px',borderRadius:'5px',backgroundColor:'#198754',padding:'8px'}}>Add Product</Link></h5>
+                <h5 className='my-5'><Link to='/manageorder' style={{color:'white', textDecoration:'none', backgroundColor:'#198754',borderRadius:'5px',padding:'8px'}}>Manage Product</Link> </h5>
+                <h5 className='my-5'><Link to='/admin' style={{color:'white', textDecoration:'none',borderRadius:'5px',backgroundColor:'#198754',padding:'8px'}}>Add Product</Link></h5>
             </div>
             <div className='w-75'>
                 <form onSubmit={handleSubmit(onSubmit)}>
